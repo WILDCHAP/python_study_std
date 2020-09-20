dc014d65ea424fb787c7d1d7f2cc0399c3065ad3
ubuntu1 <993704315@qq.com>
2020-09-20 01:08:40 -0700

    2020/09/20

--- python_project_01/com/wildchap/python/__init__.py
+++ python_project_01/com/wildchap/python/__init__.py
@@ -1,1 +1,13 @@
-print("hellow python")
+first_name = "张"
+last_name = "三"
+
+print(first_name + last_name)
+
+print(first_name * 5)
+print(last_name * 5 + "\n" + (first_name + last_name) * 5)
+
+a = input("请输入密码")
+
+print(a * 5)
+print(float(a))
+print(int(a) * 5)
---
+++ python_project_01/生成随机数.py
@@ -0,0 +1,5 @@
+import random
+import numpy as np
+
+print(random.randint(1, 100))
+print(np.random.randn(2, 2, 2))
