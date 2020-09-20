3f1e34e74c2e8fa1d6bb709b6a0be0c65a4c92ce
ubuntu1 <993704315@qq.com>
2020-09-20 02:57:18 -0700

    2020/09/20

--- python_project_01/list列表.py
+++ python_project_01/list列表.py
@@ -1,16 +1,12 @@
-import 九九乘法表 as jj
-
-jj.f1()
-print(jj.name)
-
-a = ['a', 'b', 'c', 'd', 'a', 'c']
+a = ['a', 'b', 'c', 'd', 'a', 'c', 6]
 #a.sort()
-a.sort(reverse=True)
-a.reverse()
+#a.sort(reverse=True)
+#a.reverse()
 a.insert(0,'k')
 b = set(['a', 'b', 'c', 'd', 'a', 'c'])
 print(a, b)
 print(a.index('a'))
+print(len(a), a.count(6))
 a.remove('k')
 print(a)
 del a[3]
@@ -18,4 +14,6 @@ print(a)
 k1 = a.pop()
 print(k1)
 a.append('z')
+print(a)
+a.clear()
 print(a)
---
+++ python_project_01/体验模块.py
@@ -0,0 +1,4 @@
+import 九九乘法表 as jj
+
+jj.f1()
+print(jj.name)
