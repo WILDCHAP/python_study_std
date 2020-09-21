97303a8876f516d0d653f02a8130727ea001fdf8
ubuntu1 <993704315@qq.com>
2020-09-20 20:54:31 -0700

    2020/09/21

---
+++ python_Project_03/多值参数.py
@@ -0,0 +1,10 @@
+# *args 元组  **kwargs  字典
+def demo(num, *nums, **person):
+    print(num)
+    print(nums)
+    print(person)
+
+demo(1)
+demo(1, 2, 3, 4, 5)
+demo(1, 2, 3, 4, 5, name="小明")
+demo(1, 2, 3, 4, 5, name="小明", age = 18)
